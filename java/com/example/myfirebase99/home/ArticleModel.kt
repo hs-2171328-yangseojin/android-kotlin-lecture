093a8a9d43f884aceeb14ab62d52441ae5fa17b4
hs-2171328-yangseojin <117566778+hs-2171328-yangseojin@users.noreply.github.com>
2023-12-05 19:46:34 +0900

    Update ArticleModel.kt

--- java/com/example/myfirebase99/home/ArticleModel.kt
+++ java/com/example/myfirebase99/home/ArticleModel.kt
@@ -11,6 +11,3 @@ data class ArticleModel(
 ) {
     constructor() : this(0, "", "", 0, "", "", false)
 }
-
-// Firebase RealTime Database에서 Model Class를 통해 데이터를 주고 받고 싶을 떄는
-// 반드시 위와 같이 빈 생성자를 정의해줘야 한다. -> ( 아마 null 예외처리 때문인 듯 함 )
