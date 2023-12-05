f1fabe03b01d40f68d22a3213dda734fc4c0c995
hs-2171328-yangseojin <117566778+hs-2171328-yangseojin@users.noreply.github.com>
2023-12-05 19:42:14 +0900

    Update ChatItemAdapter.kt

--- java/com/example/myfirebase99/chatdetail/ChatItemAdapter.kt
+++ java/com/example/myfirebase99/chatdetail/ChatItemAdapter.kt
@@ -15,7 +15,7 @@ import java.util.Date
 
 class ChatItemAdapter : ListAdapter<ChatItem, ChatItemAdapter.ViewHolder>(diffUtil) {
 
-    // ViewBinding을 통해 레이아웃에서 가져옴
+
     inner class ViewHolder(private val binding: ItemChatBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
@@ -44,13 +44,11 @@ class ChatItemAdapter : ListAdapter<ChatItem, ChatItemAdapter.ViewHolder>(diffUt
     companion object {
         val diffUtil = object : DiffUtil.ItemCallback<ChatItem>() {
 
-            // 현재 노출되고 있는 아이템과 새로운 아이템이 같은지 확인 ㅡ, 새로운 아이템이 들어오면 호출됨
-            // 일반적으로 키값을 통해 구분하게 됨
             override fun areItemsTheSame(oldItem: ChatItem, newItem: ChatItem): Boolean {
                 return oldItem.time == newItem.time
             }
 
-            // 현재 아이템과 새로운 아이탬의 = 여부를 확인
+         
             override fun areContentsTheSame(oldItem: ChatItem, newItem: ChatItem): Boolean {
                 return oldItem == newItem
             }
